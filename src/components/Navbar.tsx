48d683f8b52008b2baea45e20d4f60489a56cbef
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-07 07:15:09 +0700

    feat: register

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-400">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between py-2">
+          <Link href="" className="text-xl font-bold">
+            Logo
+          </Link>
+
+          <div className="flex items-center gap-8 font-medium">
+            <Link href="">Home</Link>
+            <Link href="">Profile</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
