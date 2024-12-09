7e459cad0c9022b806b10db762e23d846331a58c
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-09 14:23:45 +0700

    feat: keep login (#4)

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,10 @@
+"use client";
+const LoadingScreen = () => {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      Loading . . .
+    </main>
+  );
+};
+
+export default LoadingScreen;
